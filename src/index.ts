c7d0bbb0f45543ede7c0401f8058390bf3eb530b
Krishan Patel <krishan711@gmail.com>
2020-12-26 21:36:19 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs/promises';
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
@@ -5,6 +6,8 @@ async function run(): Promise<void> {
   try {
     const githubToken: string = core.getInput('github-token');
     const jsonFilePath: string = core.getInput('json-file-path');
+    const fileContent = await fs.readFile(jsonFilePath, 'utf8');
+    const annotations = JSON.parse(fileContent);
     const octokit = github.getOctokit(githubToken);
 
     const pullRequest = github.context.payload.pull_request;
@@ -16,7 +19,7 @@ async function run(): Promise<void> {
     }
     // const owner = github.context.repo.owner;
     // const repo = github.context.repo.repo;
-    core.info(`here: ${jsonFilePath} ${ref} ${octokit}`);
+    core.info(`here: ${jsonFilePath} ${ref} ${annotations} ${octokit}`);
   } catch (error) {
     core.setFailed(error.message);
   }
