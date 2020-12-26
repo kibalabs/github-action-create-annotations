902a3426946b14086a7690f84d80f4c0df66794e
Krishan Patel <krishan711@gmail.com>
2020-12-26 20:15:20 +0000

    .

--- webpack-config-modifier.js
+++ webpack-config-modifier.js
@@ -1,5 +1,20 @@
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
 module.exports = (config) => {
   // NOTE(krishan711): mark no packages as external so everything is bundled
   config.externals = {};
+  config.plugins = [
+    ...(config.plugins || []),
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            { source: path.join(config.output.path, 'index.js'), destination: './runnable/' },
+          ],
+        },
+      },
+    }),
+  ];
   return config;
 };
