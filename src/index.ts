c53bf0d5982a459facdce55cd5c472de57e5d918
Krishan Patel <krishan711@gmail.com>
2020-12-27 10:55:36 +0000

    Feature - check the runnable has been updated in pull_request workflow (#6)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { promises as fs } from 'fs';
 
 import { ExitCode, getInput, info as logInfo, setFailed } from '@actions/core';
-import { getOctokit, context as githubContext } from '@actions/github';
+import { getOctokit, GitHub, context as githubContext } from '@actions/github';
 
 import { createCheck, ICheck, listChecks, updateCheck } from './github-checks';
-import { ANNOTATION_LEVEL_FAILURE, ANNOTATION_LEVEL_NOTICE, ANNOTATION_LEVEL_WARNING, IAnnotation } from './model';
+import { ANNOTATION_LEVEL_FAILURE, ANNOTATION_LEVEL_NOTICE, ANNOTATION_LEVEL_WARNING, IAnnotation, IResult } from './model';
 
 const generateSummary = (failureCount: number, warningCount: number, noticeCount: number): string => {
   const messages = [];
@@ -17,7 +17,7 @@ const generateSummary = (failureCount: number, warningCount: number, noticeCount
   if (noticeCount > 0) {
     messages.push(`${noticeCount} notice(s).`);
   }
-  return messages.join(' ');
+  return messages ? messages.join(' ') : 'All good.';
 };
 
 const generateConclusion = (failureCount: number, warningCount: number, noticeCount: number): string => {
@@ -30,6 +30,32 @@ const generateConclusion = (failureCount: number, warningCount: number, noticeCo
   return 'success';
 };
 
+const processAnnotations = async (annotations: IAnnotation[], octokit: GitHub): Promise<IResult> => {
+  const failureCount = annotations.filter((annotation: IAnnotation): boolean => annotation.annotation_level === ANNOTATION_LEVEL_FAILURE).length;
+  const warningCount = annotations.filter((annotation: IAnnotation): boolean => annotation.annotation_level === ANNOTATION_LEVEL_WARNING).length;
+  const noticeCount = annotations.filter((annotation: IAnnotation): boolean => annotation.annotation_level === ANNOTATION_LEVEL_NOTICE).length;
+  const summary = generateSummary(failureCount, warningCount, noticeCount);
+  const conclusion = generateConclusion(failureCount, warningCount, noticeCount);
+  logInfo(`Summary: ${summary}`);
+  logInfo(`Conclusion: ${conclusion}`);
+
+  const ref = githubContext.payload.pull_request ? githubContext.payload.pull_request.head.sha : githubContext.sha;
+  const currentChecks = await listChecks(octokit, githubContext.repo.owner, githubContext.repo.repo, ref);
+  let currentCheck = currentChecks.find((check: ICheck): boolean => check.name === githubContext.job);
+  if (!currentCheck) {
+    currentCheck = await createCheck(octokit, githubContext.repo.owner, githubContext.repo.repo, githubContext.job, ref);
+  }
+
+  const updatePromises = [];
+  const chunkSize = 50;
+  for (let index = 0; index < annotations.length; index += chunkSize) {
+    const annotationsBatch = annotations.slice(index, index + chunkSize);
+    updatePromises.push(updateCheck(octokit, githubContext.repo.owner, githubContext.repo.repo, currentCheck.id, conclusion, currentCheck.name, summary, annotationsBatch));
+  }
+  await Promise.all(updatePromises);
+  return { failureCount, warningCount, noticeCount };
+};
+
 async function run(): Promise<void> {
   try {
     const githubToken: string = getInput('github-token', { required: true });
@@ -39,29 +65,8 @@ async function run(): Promise<void> {
     const annotations = JSON.parse(fileContent) as IAnnotation[];
     const octokit = getOctokit(githubToken);
 
-    const failureCount = annotations.filter((annotation: IAnnotation): boolean => annotation.annotation_level === ANNOTATION_LEVEL_FAILURE).length;
-    const warningCount = annotations.filter((annotation: IAnnotation): boolean => annotation.annotation_level === ANNOTATION_LEVEL_WARNING).length;
-    const noticeCount = annotations.filter((annotation: IAnnotation): boolean => annotation.annotation_level === ANNOTATION_LEVEL_NOTICE).length;
-    const summary = generateSummary(failureCount, warningCount, noticeCount);
-    const conclusion = generateConclusion(failureCount, warningCount, noticeCount);
-    logInfo(`Summary: ${summary}`);
-    logInfo(`Conclusion: ${conclusion}`);
-
-    const ref = githubContext.payload.pull_request ? githubContext.payload.pull_request.head.sha : githubContext.sha;
-    const currentChecks = await listChecks(octokit, githubContext.repo.owner, githubContext.repo.repo, ref);
-    let currentCheck = currentChecks.find((check: ICheck): boolean => check.name === githubContext.job);
-    if (!currentCheck) {
-      currentCheck = await createCheck(octokit, githubContext.repo.owner, githubContext.repo.repo, githubContext.job, ref);
-    }
-
-    const updatePromises = [];
-    const chunkSize = 50;
-    for (let index = 0; index < annotations.length; index += chunkSize) {
-      const annotationsBatch = annotations.slice(index, index + chunkSize);
-      updatePromises.push(updateCheck(octokit, githubContext.repo.owner, githubContext.repo.repo, currentCheck.id, conclusion, currentCheck.name, summary, annotationsBatch));
-    }
-    await Promise.all(updatePromises);
-    if (failOnError && failureCount > 0) {
+    const result = await processAnnotations(annotations, octokit);
+    if (failOnError && result.failureCount > 0) {
       process.exitCode = ExitCode.Failure;
     }
   } catch (error) {
